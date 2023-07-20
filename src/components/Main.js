5acf2b395ff4ba31bd6c0f66e8b4c8f903bb71cb
Zoltan Pozsonyi <pozsonyizoli@gmail.com>
2023-07-20 14:24:03 +0200

    finetuning ui + feature to change step order

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,11 +2,13 @@ import { useReducer } from "react";
 import TransformationStep from "./TransformationStep";
 import { Dropdown } from "react-bootstrap";
 import Button from '@mui/material/Button';
+import Switch from '@mui/material/Switch';
 
 const initialState = {
     inputText: "",
     outputText: "",
-    steps: []
+    steps: [],
+    processLineByLine: true
 }
 
 function reducer(state, { type, payload }) {
@@ -27,10 +29,28 @@ function reducer(state, { type, payload }) {
                 }
                 )
             }
+        case "moveStepUp":
+            const indexOfElement = state.steps.findIndex(element => element.id === payload)
+            if(indexOfElement > 0) {
+                const tempArray = [...state.steps]
+                const elementToMove = tempArray.splice(indexOfElement, 1)[0]
+                tempArray.splice(indexOfElement-1, 0, elementToMove)
+                return { ...state, steps: tempArray}
+            }
+            return state
+        case "moveStepDown":
+            const indexOfElement2 = state.steps.findIndex(element => element.id === payload)
+            if(indexOfElement2 < state.steps.length-1) {
+                const tempArray = [...state.steps]
+                const elementToMove = tempArray.splice(indexOfElement2, 1)[0]
+                tempArray.splice(indexOfElement2+1, 0, elementToMove)
+                return { ...state, steps: tempArray}
+            }
+            return state
         case "closeStep":
             return { ...state, steps: state.steps.filter(item => item.id !== payload) }
         case "executeTransformation":
-            let text = state.inputText.split('\n')
+            let text = state.processLineByLine?state.inputText.split('\n'):[state.inputText]
 
             for (const step of state.steps) {
                 text = text.map(line => {
@@ -42,7 +62,8 @@ function reducer(state, { type, payload }) {
             }
 
             return { ...state, outputText: text.join('\n') }
-
+        case "toggleLineByLineProcessing":
+            return { ...state, processLineByLine: !state.processLineByLine }        
         default:
             throw new Error("unsupported operation")
     }
@@ -50,38 +71,55 @@ function reducer(state, { type, payload }) {
 }
 
 export default function Main() {
-    const [{ inputText, outputText, steps }, dispatcher] = useReducer(reducer, initialState);
+    const [{ inputText, outputText, steps, processLineByLine }, dispatcher] = useReducer(reducer, initialState);
 
 
     return <main>
-        <div>
-            <p className="prevent-select label">Input</p>
+        <div className="text-area-holder">
+            <p className="prevent-select title">Input</p>
+            <div></div>
             <textarea className="custom-text-area" value={inputText} onChange={(event) => dispatcher({ type: "newInputText", payload: event.target.value })} />
         </div>
-        <div className="step-holder">
-            <p className="prevent-select label">Steps</p>
+        <div className="step-section">
+            <div className="prevent-select title">Steps</div>
+            <div >
+                <div style={{display: "inline"}}>
+                    <span>Execute for each line separately</span>
+                    <Switch checked={processLineByLine}
+                        onChange={()=>dispatcher({type:"toggleLineByLineProcessing"})}
+                        inputProps={{ 'aria-label': 'controlled' }}
+                    />
+                </div>
+            </div>
+            <div className="step-holder">
+                {steps.map((step) => <TransformationStep key={step.id} id={step.id} dispatcher={dispatcher} type={step.type} />)}
+            </div>
+            <div className="step-action-holder">
+                <Dropdown style={{ width: "50%", alignSelf: "center" }}>
+                    <Dropdown.Toggle variant="success" disabled={steps.length >= 5}>
+                        Add transformation
+                    </Dropdown.Toggle>
 
-            {steps.map((step) => <TransformationStep key={step.id} id={step.id} dispatcher={dispatcher} type={step.type} />)}
-            <Dropdown style={{ width: "50%", alignSelf: "center" }}>
-                <Dropdown.Toggle variant="success" disabled={steps.length >= 5}>
-                    Add transformation
-                </Dropdown.Toggle>
-
-                <Dropdown.Menu>
-                    <Dropdown.Item onClick={() => dispatcher({ type: "newTransformation", payload: "replace" })}>Replace</Dropdown.Item>
-                    <Dropdown.Item onClick={() => dispatcher({ type: "newTransformation", payload: "regex" })}>Regular expression</Dropdown.Item>
-                    <Dropdown.Item onClick={() => dispatcher({ type: "newTransformation", payload: "append" })}>Append</Dropdown.Item>
-                    <Dropdown.Item onClick={() => dispatcher({ type: "newTransformation", payload: "substring" })}>Substring</Dropdown.Item>
-                    <Dropdown.Item onClick={() => dispatcher({ type: "newTransformation", payload: "insert" })}>Insert</Dropdown.Item>
-                </Dropdown.Menu>
-            </Dropdown>
-
-            <Button style={{ width: "50%", alignSelf: "center" }} size="large" variant="contained" disabled={steps.length === 0} onClick={() => dispatcher({ type: "executeTransformation" })}>Transform</Button>
+                    <Dropdown.Menu>
+                        <Dropdown.Item onClick={() => dispatcher({ type: "newTransformation", payload: "replace" })}>Replace</Dropdown.Item>
+                        <Dropdown.Item onClick={() => dispatcher({ type: "newTransformation", payload: "regex" })}>Regular expression</Dropdown.Item>
+                        <Dropdown.Item onClick={() => dispatcher({ type: "newTransformation", payload: "append" })}>Append</Dropdown.Item>
+                        <Dropdown.Item onClick={() => dispatcher({ type: "newTransformation", payload: "substring" })}>Substring</Dropdown.Item>
+                        <Dropdown.Item onClick={() => dispatcher({ type: "newTransformation", payload: "insert" })}>Insert</Dropdown.Item>
+                        <Dropdown.Item onClick={() => dispatcher({ type: "newTransformation", payload: "upper" })}>To uppercase</Dropdown.Item>
+                        <Dropdown.Item onClick={() => dispatcher({ type: "newTransformation", payload: "lower" })}>To lowercase</Dropdown.Item>
+                    </Dropdown.Menu>
+                </Dropdown>
+                <Button style={{ width: "50%", alignSelf: "center" }} size="large" variant="contained" disabled={steps.length === 0} onClick={() => dispatcher({ type: "executeTransformation" })}>Transform</Button>
+            </div>
         </div>
-        <div>
-            <p className="prevent-select label">Output</p>
+        <div className="text-area-holder">
+            <p className="prevent-select title">Output</p>
+            <div>
+                <Button>Copy</Button>
+                <Button>Move to input</Button>
+            </div>
             <textarea className="custom-text-area" value={outputText} readOnly />
-            <Button>Copy</Button>
         </div>
 
     </main>
--- src/components/TransformationStep.js
+++ src/components/TransformationStep.js
@@ -4,6 +4,9 @@ import DeleteIcon from '@mui/icons-material/Delete';
 import TextField from '@mui/material/TextField';
 import DisabledByDefaultIcon from '@mui/icons-material/DisabledByDefault';
 import { Button } from "@mui/material";
+import ArrowUpwardIcon from '@mui/icons-material/ArrowUpward';
+import ArrowDownwardIcon from '@mui/icons-material/ArrowDownward';
+import { ArrowDownward } from "@mui/icons-material";
 
 const TEXT_ALIGNMENT = {
     sx: {
@@ -13,9 +16,6 @@ const TEXT_ALIGNMENT = {
     }
 }
 
-
-
-
 export default function TransformationStep({ id, type, dispatcher }) {
 
     const [fromValue, setFromValue] = useState('')
@@ -24,12 +24,12 @@ export default function TransformationStep({ id, type, dispatcher }) {
 
     function fromField(label, numberOnly=false, width=20){
         return <TextField type={numberOnly?"number":"text" } label={label} InputProps={TEXT_ALIGNMENT} style={{width: `${width}%`}}
-        size="small" value={fromValue} onChange={(event) => setFromValue(event.target.value)} />
+        size="small" value={fromValue} onChange={(event) => setFromValue(event.target.value)} disabled={disabled}/>
     }
 
     function toField(label,numberOnly=false,  width=20) {
         return <TextField type={numberOnly?"number":"text" } label={label} InputProps={TEXT_ALIGNMENT} style={{width: `${width}%`}}
-            size="small" value={toValue} onChange={(event) => setToValue(event.target.value)} />
+            size="small" value={toValue} onChange={(event) => setToValue(event.target.value)} disabled={disabled}/>
     }
 
     const TYPES = {
@@ -85,7 +85,6 @@ export default function TransformationStep({ id, type, dispatcher }) {
             name: "Substring",
             transformer: function (fromIndex, toIndex) {
                 return function transform(input) {
-                    console.log(input)
                     if(toIndex) { 
                         return input.substr(Number(fromIndex), Number(toIndex))
                     } else {
@@ -97,6 +96,25 @@ export default function TransformationStep({ id, type, dispatcher }) {
             {fromField('Start(inclusive)', true)}
             {toField('Length', true)}
             </>
+        },
+        "upper": {
+            name: "Transform to uppercase",
+            transformer: function () {
+                return function transform(input) {
+                    return input.toUpperCase()
+                }
+            },
+             /*for some reason returning a fragment or a div here causes infinite render*/
+            form: <div></div>
+        },
+        "lower": {
+            name: "Transform to lowercase",
+            transformer: function () {
+                return function transform(input) {
+                    return input.toLowerCase()
+                }
+            },
+            form: <></>
         }
     }
 
@@ -108,6 +126,8 @@ export default function TransformationStep({ id, type, dispatcher }) {
             case "append": transformer = TYPES[type].transformer(fromValue)
             case "substring": transformer = TYPES[type].transformer(fromValue, toValue)
             case "insert": transformer = TYPES[type].transformer(fromValue, toValue)
+            case "upper": transformer = TYPES[type].transformer()
+            case "lower": transformer = TYPES[type].transformer()
         }
         dispatcher({
                 type: "updateTransformation", payload: {
@@ -118,17 +138,23 @@ export default function TransformationStep({ id, type, dispatcher }) {
                 }
             })
         }
-        , [fromValue, toValue, dispatcher, id, disabled, TYPES, type])
+        , [fromValue, toValue, dispatcher, id, disabled, type])
 
 
     return <div style={{ position: "relative" ,width: "100%"}}>
         <div className={disabled ? "step disabled-step prevent-select" : "step prevent-select"} style={{ position: "absolute", display: "flex", flexDirection: "row", gap: "5px", opacity: disabled ? 0.3 : 1 }}>
+            <IconButton onClick={() => dispatcher({type:"moveStepUp", payload: id})} aria-label="moveup" disabled={disabled}>
+                <ArrowUpwardIcon/>
+            </IconButton>
+            <IconButton onClick={() => dispatcher({type:"moveStepDown", payload: id})} aria-label="movedown" disabled={disabled}>
+                <ArrowDownward/>
+            </IconButton>
             <div className="prevent-select label" style={{width: "40%"}}>{TYPES[type].name}</div>
             {TYPES[type].form}
-            <IconButton onClick={() => setDisabled(true)} aria-label="delete" disabled={disabled}>
+            <IconButton onClick={() => setDisabled(true)} aria-label="disable" disabled={disabled}>
                 <DisabledByDefaultIcon />
             </IconButton>
-            <IconButton onClick={() => dispatcher({ type: 'closeStep', payload: id })} aria-label="delete">
+            <IconButton onClick={() => dispatcher({ type: 'closeStep', payload: id })} aria-label="delete" disabled={disabled}>
                 <DeleteIcon />
             </IconButton>
         </div>
